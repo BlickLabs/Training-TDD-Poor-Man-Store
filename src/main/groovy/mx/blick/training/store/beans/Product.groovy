7a210e4061f864febfd02bc580c59b7e15789d34
Juan Resendiz <jresendiz27@gmail.com>
2017-02-25 12:18:11 -0600

    #4, Create Product bean class

---
+++ src/main/groovy/mx/blick/training/store/beans/Product.groovy
@@ -0,0 +1,12 @@
+package mx.blick.training.store.beans
+
+/**
+ * Created by jresendiz on 25/02/17.
+ */
+class Product {
+    String uniqueId
+    String name
+    Integer quantity
+    String storeName
+    Date creationDate
+}
