166687de2ab40d643bf93f5cd7eb34e84bfbda35
Juan Resendiz <jresendiz27@gmail.com>
2017-02-25 15:24:03 -0600

    #4, Refactoring filemanagement class

--- src/main/groovy/mx/blick/training/store/IO/FileManagement.groovy
+++ src/main/groovy/mx/blick/training/store/IO/FileManagement.groovy
@@ -7,7 +7,7 @@ package mx.blick.training.store.IO
 class FileManagement {
     final String DATABASE_NAME = "database.csv"
     final String PATH = "/home/jresendiz/Desktop/"
-    final List CSV_HEADER = ["uniqueId","name", "quantity", "creationDate", "storeName"]
+    final static List CSV_HEADER = ["uniqueId","name", "quantity", "creationDate", "storeName"]
     File createDatabaseFile(String path = "$PATH$DATABASE_NAME", List header = CSV_HEADER) throws Exception {
         def database = new File(path)
         if(!database.exists()) {
